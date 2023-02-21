da7cc247e73c3920c6c002cd88db6630c5f9540f
Mihai Postelnicu <mihaipostelnicu@192-168-0-101.rdsnet.ro>
2023-02-21 05:11:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User />
+      <User />
+      <User />
     </div>
   );
 }
+
+const User = () => {
+  return <h1>Mihai</h1>
+};
 
 export default App;
