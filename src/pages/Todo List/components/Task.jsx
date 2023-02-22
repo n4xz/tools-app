c757fb82fdc7fb4ad43d4ea4e3407a33a83e7344
n4xz <n4xz@icloud.com>
2023-02-22 19:14:44 +0200

    small changes

--- src/pages/Todo List/components/Task.jsx
+++ src/pages/Todo List/components/Task.jsx
@@ -5,7 +5,7 @@ export const Task = (props) => {
 			<div className='flex gap-3'>
 				<button
 					style={{
-						backgroundColor: props.completed ? 'green' : 'transparent', borderRadius:'100%'
+						backgroundColor: props.completed ? 'green' : 'transparent', borderRadius:'20px'
 					}}
 					onClick={() => props.completeTask(props.id)}
 					className='h-5 w-5 rounded-full text-xs outline outline-[1px]'
